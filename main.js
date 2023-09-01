ac0da1a35f8206e567a4417b3b4c9e4e2677f8a7
cotabas <cotabas@gmail.com>
2023-09-01 00:47:38 -0700

    sha256

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-import './main.css'
+import { sha256 } from 'js-sha256';
+
+console.log("jhey")
+
+var today = new Date()
+
+for (let m = 1; m <= 12; m++) {
+  mm = String(m)
+  if (mm.length == 1) { mm = "0" + mm }
+
+  for (let d = 1; d <= 31; d++) {
+    dd = String(d)
+    if (dd.length == 1) { dd = "0" + dd }
+    
+    today = `${mm}/${dd}/2023`
+
+    console.log(sha256(today))
+
+  }
 
 
+}
